17f6e280b0f5f82e880b2df597557c7d769aaf39
Chirag Chouhan <76790227+chirag-23@users.noreply.github.com>
2023-01-12 01:39:25 +0530

    Update [id].jsx

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -101,7 +101,7 @@ const Product = ({ pizza }) => {
 
 export const getServerSideProps = async ({ params }) => {
   const res = await axios.get(
-    `http://localhost:3000/api/products/${params.id}`
+    `https://cherrypizzas.vercel.app/api/products/${params.id}`
   );
   return {
     props: {
