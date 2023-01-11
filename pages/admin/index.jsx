3d1a2c37ba76050522660515ab1208bcdfb68c49
Chirag Chouhan <76790227+chirag-23@users.noreply.github.com>
2023-01-12 01:41:15 +0530

    Update index.jsx

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -16,7 +16,7 @@ const Index = ({ orders, products, admin }) => {
     console.log(id);
     try {
       const res = await axios.delete(
-        "http://localhost:3000/api/products/" + id
+        "https://cherrypizzas.vercel.app/api/products/" + id
       );
       setPizzaList(pizzaList.filter((pizza) => pizza._id !== id));
     } catch (err) {
@@ -29,7 +29,7 @@ const Index = ({ orders, products, admin }) => {
     const currentStatus = item.status;
 
     try {
-      const res = await axios.put("http://localhost:3000/api/orders/" + id, {
+      const res = await axios.put("https://cherrypizzas.vercel.app/api/orders/" + id, {
         status: currentStatus + 1,
       });
       setOrderList([
@@ -140,8 +140,8 @@ export const getServerSideProps = async (ctx) => {
     };
   }
 
-  const productRes = await axios.get("http://localhost:3000/api/products");
-  const orderRes = await axios.get("http://localhost:3000/api/orders");
+  const productRes = await axios.get("https://cherrypizzas.vercel.app/api/products");
+  const orderRes = await axios.get("https://cherrypizzas.vercel.app/api/orders");
 
   return {
     props: {
